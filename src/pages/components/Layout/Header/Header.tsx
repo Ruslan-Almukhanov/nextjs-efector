d638c1b39f6c34ddb6f0896f3cc1f7ca5def756e
ruslan.almukhanov <ruslan.almukhanov@samgau.com>
2023-03-30 15:03:19 +0600

    feat: pages

---
+++ src/pages/components/Layout/Header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/users">Users</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/todos">Todos</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
