19c1324bd0c842cf8ff297187c1d1321628b0e5a
Zhanbolat Bekmaganbetov <180107180@stu.sdu.edu.kz>
2023-03-31 16:35:03 +0600

    refactor: swap names for Redirect and Login pages

--- src/pages/auth/redirect/index.tsx
+++ src/pages/auth/redirect/index.tsx
@@ -1,45 +1,5 @@
-import Link from "next/link";
-import PocketBase, { AuthProviderInfo } from "pocketbase";
-import { useEffect, useState } from "react";
-
-const pb = new PocketBase("http://127.0.0.1:8090");
-const redirectUrl = "/redirect";
-
 const RedirectPage = () => {
-  const [authProviders, setAuthProviders] = useState<AuthProviderInfo[]>([]);
-
-  async function loadLinks() {
-    const authMethods = await pb
-      .collection("users")
-      .listAuthMethods({ $autoCancel: false });
-
-    setAuthProviders(authMethods.authProviders);
-  }
-
-  function handleProviderClick(provider: AuthProviderInfo) {
-    localStorage.setItem("provider", JSON.stringify(provider));
-    console.log("handleProviderClick", authProviders);
-  }
-
-  useEffect(() => {
-    loadLinks();
-  }, []);
-
-  return (
-    <div>
-      {authProviders.length ? (
-        authProviders.map((authProvider: AuthProviderInfo) => (
-          <li key={authProvider.state} onClick={() => handleProviderClick}>
-            <Link href={authProvider.authUrl + redirectUrl}>
-              {authProvider.name}
-            </Link>
-          </li>
-        ))
-      ) : (
-        <li>No providers</li>
-      )}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default RedirectPage;
